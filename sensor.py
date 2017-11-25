b8ada6681f100672c68583cbb360a30362058d53
Kevin Yokley <kyokley2@gmail.com>
2017-11-25 14:59:23 -0600

    Adding celsius and fahrenheit methods (WIP)

--- sensor.py
+++ sensor.py
@@ -1,27 +1,59 @@
 import os
+from datetime import datetime, timedelta
+
+MINIMUM_UPDATE_TIME = 30
+
+class SensorException(Exception):
+    pass
 
 class Sensor(object):
     def __init__(self):
         self._bus = None
-        self._setup()
+        self._cached_temp = None
+        self._last_update = None
+
+        try:
+            self._setup()
+        except SensorException as e:
+            print(e)
+
+    def is_initialized(self):
+        return bool(self._bus)
 
     def _setup(self):
+        if self._bus:
+            raise SensorException('Sensor is already initialized')
+
         for x in os.listdir('/sys/bus/w1/devices'):
             if x != 'w1_bus_master1':
                 self._bus = x
                 break
 
         if not self._bus:
-            raise Exception('Failed to find temperature sensor')
+            raise SensorException('Failed to find temperature sensor')
 
-    def read(self):
-        location = '/sys/bus/w1/devices/{}/w1_slave'.format(self._bus)
+    def _read(self):
+        if not self._bus:
+            raise SensorException('Sensor is not initialized')
 
-        with open(location) as tfile:
-            text = tfile.read()
+        if (not self._cached_temp or
+                not self._last_update or
+                (self._last_update + timedelta(seconds=MINIMUM_UPDATE_TIME) < datetime.now())):
+            location = '/sys/bus/w1/devices/{}/w1_slave'.format(self._bus)
 
-        secondline = text.split("\n")[1]
-        temperaturedata = secondline.split(" ")[9]
-        temperature = float(temperaturedata[2:])
-        temperature = temperature / 1000.0
-        return temperature
+            with open(location) as tfile:
+                text = tfile.read()
+
+            secondline = text.split("\n")[1]
+            temperaturedata = secondline.split(" ")[9]
+            temperature = float(temperaturedata[2:])
+            temperature = temperature / 1000.0
+            return temperature
+        else:
+            return self._cached_temp
+
+    def get_celsius(self):
+        return self._read()
+
+    def get_fahrenheit(self):
+        return self.get_celsius() * (9.0 / 5.0) + 32
--- server.py
+++ server.py
@@ -1,19 +1,33 @@
-#from flask import (Flask,
-#                   Response,
-#                   request,
-#                   send_file,
-#                   render_template,
-#                   jsonify)
+import mock
 
-from flask import Flask
+from flask import (Flask,
+                   jsonify,
+                   )
 from werkzeug.contrib.fixers import ProxyFix
+from sensor import Sensor
 
 app = Flask(__name__, static_url_path='')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
+#sensor = Sensor()
+sensor = mock.MagicMock(Sensor)
+sensor.is_initialized.return_value = True
+sensor._read.return_value = 50
+sensor.get_celsius.return_value = 50
+sensor.get_fahrenheit.return_value = 75
+
 @app.route('/status/', methods=['GET'])
+@app.route('/status', methods=['GET'])
 def status():
-    pass
+    res = {'status': sensor.is_initialized()}
+    return jsonify(res)
+
+@app.route('/', methods=['GET'])
+def get_temp():
+    res = {'celsius': sensor.get_celsius(),
+           'fahrenheit': sensor.get_fahrenheit(),
+           }
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.debug = True
