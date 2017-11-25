bae78c48cb5a849b2f7bd493344c410eaefe411c
Kevin Yokley <kyokley2@gmail.com>
2017-11-25 15:00:36 -0600

    Removing mocks from real code

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-import mock
-
 from flask import (Flask,
                    jsonify,
                    )
@@ -9,12 +7,7 @@ from sensor import Sensor
 app = Flask(__name__, static_url_path='')
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
-#sensor = Sensor()
-sensor = mock.MagicMock(Sensor)
-sensor.is_initialized.return_value = True
-sensor._read.return_value = 50
-sensor.get_celsius.return_value = 50
-sensor.get_fahrenheit.return_value = 75
+sensor = Sensor()
 
 @app.route('/status/', methods=['GET'])
 @app.route('/status', methods=['GET'])
